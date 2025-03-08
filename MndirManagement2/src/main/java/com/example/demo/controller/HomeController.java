88566754fcf2734afdcabec51d1c63c917a4b16a
rockbandstyle123 <rockbandstyle123@gmail.com>
2025-03-08 17:38:21 +0530

    setter and getters for product class fields

--- MndirManagement2/src/main/java/com/example/demo/controller/HomeController.java
+++ MndirManagement2/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Product;
 
 @RestController
 public class HomeController {
@@ -12,6 +15,12 @@ public class HomeController {
 	  {
 		  return "Now Successful...!!";
 		  
+	  }
+	  
+	  @GetMapping("/lodge")
+	  public String saveData(@RequestBody Product p) {
+		  
+		  return "Data inserted";
 	  }
 	 
 }
